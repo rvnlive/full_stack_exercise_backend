d49acb1232396a0db109197c7a7bc7b68d1ef6f6
Richard Oven <71844377+rvnlive@users.noreply.github.com>
2021-09-12 20:42:15 +0100

    CORS setup

--- app.js
+++ app.js
@@ -8,8 +8,19 @@ const cors = require('cors')
 const userRoutes = require('./app/routes/userRoutes')
 const favouriteRoutes = require('./app/routes/favouriteRoutes')
 
+const allowedOrigins = 'https://synthetix-exercise.herokuapp.com/'
 app.use(cors({
-  origin: 'https://synthetix-exercise.herokuapp.com/'
+  origin: function (origin, callback) {
+    // allow requests with no origin
+    // (like mobile apps or curl requests)
+    if (!origin) return callback(null, true)
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.'
+      return callback(new Error(msg), false)
+    }
+    return callback(null, true)
+  }
 }))
 /** Postgres + Sequelize Connection */
 const database = require('./app/config/database.config')
