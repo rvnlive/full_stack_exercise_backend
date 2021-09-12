f5b048239454d57580a401726cbfa195dcf6f475
Richard Oven <71844377+rvnlive@users.noreply.github.com>
2021-09-12 20:59:10 +0100

    Method changes

--- app/routes/favouriteRoutes.js
+++ app/routes/favouriteRoutes.js
@@ -4,8 +4,8 @@ const auth = require('../helpers/authorization')
 
 const favouriteController = require('../controllers/favouriteController')
 
-router.post('/books', auth, favouriteController.getFavouriteBooks)
-router.post('/movies', auth, favouriteController.getFavouriteMovies)
+router.get('/books', auth, favouriteController.getFavouriteBooks)
+router.get('/movies', auth, favouriteController.getFavouriteMovies)
 router.post('/add', auth, favouriteController.addToFavourites)
 router.delete('/remove', auth, favouriteController.removeFromFavourites)
 
