4a35ef6b0f1a7653358a3ee9286a1268ed362259
Richard Oven <71844377+rvnlive@users.noreply.github.com>
2021-09-12 20:54:32 +0100

    CORS change

--- app.js
+++ app.js
@@ -3,15 +3,16 @@ const app = express()
 const helmet = require('helmet')
 const bodyParser = require('body-parser')
 require('dotenv').config()
-const cors = require('cors')
 
 const userRoutes = require('./app/routes/userRoutes')
 const favouriteRoutes = require('./app/routes/favouriteRoutes')
 
-app.use(cors({
-  origin: '*',
-  methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH']
-}))
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS')
+  next()
+})
 /** Postgres + Sequelize Connection */
 const database = require('./app/config/database.config')
 database.authenticate()
